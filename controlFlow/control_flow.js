e0913aa6038d7605c0cab3e5a7dc4b6208d025cb
babaluis <luisbaba62@gmail.com>
2025-08-21 08:23:31 +0100

    usertype and al js changes

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,47 @@
+let userRole = "admin";
+let accessLevel;
+let isLoggedIn = true;
+let userMessage;
+let userType = "subscriber";
+let userCategory;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+    console.log("Access Level:", accessLevel);
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+    console.log("Access Level:", accessLevel);
+} else {
+    accessLevel = "No access granted";
+    console.log("Access Level:", accessLevel);
+}
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+        console.log("User Message:", userMessage);
+    } else {
+        userMessage = "Welcome, User!";
+        console.log("User Message:", userMessage);
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+    console.log("User Message:", userMessage);
+}
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        console.log("User Category:", userCategory);
+        break;
+    case "manager":
+        userCategory = "Manager";
+        console.log("User Category:", userCategory);
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        console.log("User Category:", userCategory);
+        break;
+    default:
+        userCategory = "Unknown";
+}
