48d97786be858732ef9ed309af60befbdd37d2c1
babaluis <luisbaba62@gmail.com>
2025-08-31 20:34:10 +0100

    todo list

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -42,3 +42,19 @@ addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
 clearAlldBtn.addEventListener("click", clearAllTasks);
 
+// Check if the browser supports localStorage
+function isLocalStorageSupported() {
+  try {
+    const testKey = '__test__';
+    localStorage.setItem(testKey, testKey);
+    localStorage.removeItem(testKey);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+if (isLocalStorageSupported()) {
+  console.log('localStorage is supported in this browser!');
+} else {
+  console.log('Sorry, localStorage is not supported in this browser.');
+}
